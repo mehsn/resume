d5e434e7745dfe1665735fc65b2b09d6f9f6766a
mehsn <ihsnun@gmail.com>
2020-07-21 14:30:07 +0430

    Updates

---
+++ precache-manifest.06f837f932b28fff3bc9ca83eb4d812c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36830d9e6b93d05fd3997bb16cdcecf8",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "bb0da400d2eb96b7df6f",
+    "url": "/resume/static/css/main.38f69ae9.chunk.css"
+  },
+  {
+    "revision": "95af4b459f111d35f4ac",
+    "url": "/resume/static/js/2.26698d10.chunk.js"
+  },
+  {
+    "revision": "e4d0b7b6ee221c9758b548593a6bc2ac",
+    "url": "/resume/static/js/2.26698d10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb0da400d2eb96b7df6f",
+    "url": "/resume/static/js/main.91556542.chunk.js"
+  },
+  {
+    "revision": "8c1d914a1677cea53979",
+    "url": "/resume/static/js/runtime-main.75d8c91e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/resume/static/media/logo.5d5d9eef.svg"
+  }
+]);
